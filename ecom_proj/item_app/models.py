eb4bba65d2c3f1a9b641641dac81227b85356dfe
Tim Ludwiczak <tim.ludwiczak@gmail.com>
2024-08-05 15:36:43 -0500

    updates

--- ecom_proj/item_app/models.py
+++ ecom_proj/item_app/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
